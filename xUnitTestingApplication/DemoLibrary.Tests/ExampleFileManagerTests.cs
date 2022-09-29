79e74db0544b52fc1fd7c511300b612f14d35b1e
jcrowe91 <jcrowe9119@gmail.com>
2022-09-28 22:28:01 -0500

    Added ExampleFileManagerTests

---
+++ xUnitTestingApplication/DemoLibrary.Tests/ExampleFileManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Tests
+{
+    public class ExampleFileManagerTests
+    {
+        [Fact]
+        public void ExampleLoadTextFile_ValidNameShouldWork()
+        {
+            string actual = ExampleFileManager.ExampleLoadTextFile("This is a valid file name.");
+            Assert.True(actual.Length > 0);
+        }
+
+        [Fact]
+        public void ExampleLoadTextFile_InvalidNameShouldFail()
+        {
+            Assert.Throws<FileNotFoundException>(() => ExampleFileManager.ExampleLoadTextFile(""));
+        }
+    }
+}
