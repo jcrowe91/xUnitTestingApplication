b63b836319dd4a3f937b2c319a0368ab2afe1616
jcrowe91 <jcrowe9119@gmail.com>
2022-09-28 22:20:35 -0500

    Added ExampleFileManager class

---
+++ xUnitTestingApplication/DemoLibrary/ExampleFileManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class ExampleFileManager
+    {
+        public static string ExampleLoadTextFile(string file)
+        {
+            if (file.Length < 10)
+            {
+                throw new System.IO.FileNotFoundException("The file was of improper length.");
+            }
+
+            return "The file was loaded correctly";
+        }
+    }
+}
